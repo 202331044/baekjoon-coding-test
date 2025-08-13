e962e6e0ac6869ec7ef369630c0e9ad2efb3f9f6
202331044 <131847052+202331044@users.noreply.github.com>
2025-08-13 11:04:39 +0900

    refactor: simplify code by removing unnecessary parts

--- 3273.cpp
+++ 3273.cpp
@@ -9,7 +9,6 @@ int main()
 	vector<int> vec;
 	int n, tmp, x;
 	int result = 0;
-  
 	cin >> n;
 
 	for (int i = 0; i < n; ++i)
@@ -29,29 +28,12 @@ int main()
 	{
 		if (vec[left] + vec[right] == x)
 		{
-			int leftVal = vec[left];
-			int rightVal = vec[right];
-			int leftCnt = 0, rightCnt = 0;
-
-			while (left < right && vec[left] == leftVal)
-			{
-				left++;
-				leftCnt++;
-			}
-
-			while (right >= left && vec[right] == rightVal)
-			{
-				right--;
-				rightCnt++;
-			}
-
-			if (leftVal == rightVal)
-				result += leftCnt * (leftCnt - 1)/2;
-			else
-				result += leftCnt * rightCnt;
+			result++;
+			left++;
+			right--;
 			continue;
 		}
-
+			
 		if (vec[left] + vec[right] < x)
 			left++;
 		else
@@ -59,6 +41,6 @@ int main()
 	}
 
 	cout << result;
-  
+
 	return 0;
 }
