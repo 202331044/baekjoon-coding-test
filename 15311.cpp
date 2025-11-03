52208a675ea8c1b5749a618e992f2d661a8991c9
202331044 <131847052+202331044@users.noreply.github.com>
2025-11-03 10:38:45 +0900

    feat: solve 15311 - 해 구성하기 9

---
+++ 15311.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << 2000 << "\n";
+
+	for (int i = 0; i < 1000; ++i)
+		cout << 1 << " ";
+	for (int j = 1000; j < 2000; ++j)
+		cout << 1000 << " ";
+
+	return 0;
+}
